39452f28c5f9f0d186f13867c8b067062ab9965a
Julian Andres Arbelaez Cadavid <47003786+julianDMC4@users.noreply.github.com>
2019-02-06 23:07:52 -0500

    Add files via upload

---
+++ actividad clase -the- .cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividad_clase_grupos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "the quick brown fox jumps over the lazy dog";
+            string salida = "";
+            int contador = 0;
+            string palabra = "the";
+
+            for (int i = 0; i < texto.Length; i++)
+            {
+                if (texto[i]!= ' ')
+                {
+                    salida += texto[i];
+                }
+                else if (texto[i]== ' ')
+                {
+                    if (salida == palabra)
+                    {
+                        contador += 1;
+                        
+                    }
+                    salida = "";
+
+                  
+                }
+              
+            }
+              Console.WriteLine(texto);
+                Console.WriteLine("cantidad de the que salen:::" + contador);
+        }
+    }
+}
